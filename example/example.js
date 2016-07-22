a967bb558f9b2e4bf1a231f2ba263cc2070c4a0b
Michael Cramm <mike@petrofeed.com>
2016-07-22 12:56:33 -0600

    Construct example date in a "legal" format.
    Technically '2016-07-06 00:00:01' is not a legal date format to `new
    Date()`, though Chrome seems to allow it.

--- example/example.js
+++ example/example.js
@@ -3,8 +3,8 @@ var timelineChart = require('../main');
 d3.json('http://localhost:8080/sample.json', function (data) {
   var chartElement = document.getElementById('chart');
   var opts = {
-    minDate: new Date('2016-07-06 00:00:01'),
-    maxDate: new Date('2016-07-06 23:59:59')
+    minDate: new Date(2016, 6, 6, 0, 0, 1),
+    maxDate: new Date(2016, 6, 6, 23, 59, 59)
   };
 
  timelineChart(chartElement, data, opts);
