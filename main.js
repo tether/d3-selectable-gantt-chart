679d3e89420cce294bcfecddebbc2a03eb37c472
Nicholas Pufal <nicholas@petrofeed.com>
2016-07-22 11:35:20 -0600

    Fix code alignment

--- main.js
+++ main.js
@@ -36,15 +36,13 @@ function initialize (element, data, opts) {
 }
 
 var createChart = function (element, data, opts) {
-  opts = initialize(element, data, opts);
-  var labels = extractLabels(data);
-  var baseSVG = d3.select(element)
-                  .append('svg')
-                  .attr('width', opts.width);
-  var chartData = baseSVG.append('g').attr('id', 'chart-data');
+  opts               = initialize(element, data, opts);
 
-  var chartHeight = labels.length * opts.barHeight;
-  var selectionInfo = d3.select('#selection-info').append('ul');
+  var labels         = extractLabels(data);
+  var baseSVG        = d3.select(element).append('svg').attr('width', opts.width);
+  var chartData      = baseSVG.append('g').attr('id', 'chart-data');
+  var chartHeight    = labels.length * opts.barHeight;
+  var selectionInfo  = d3.select('#selection-info').append('ul');
   var activitiesInfo = d3.select('#selection-info').append('dl');
 
   function updateSelectionInfo (timeRange) {
@@ -65,10 +63,10 @@ var createChart = function (element, data, opts) {
   }
 
   function brushed () {
-    var timeRange = brush.extent();
-    var bars = d3.selectAll('rect.bar');
+    var timeRange  = brush.extent();
+    var bars       = d3.selectAll('rect.bar');
     var brushStart = Math.floor(timeRange[0].getTime() / 1000);
-    var brushEnd = Math.floor(timeRange[1].getTime() / 1000);
+    var brushEnd   = Math.floor(timeRange[1].getTime() / 1000);
 
     updateSelectionInfo(timeRange);
 
@@ -100,9 +98,9 @@ var createChart = function (element, data, opts) {
   }
 
   var timeScale = d3.time
-                     .scale()
-                     .domain([opts.minDate, opts.maxDate])
-                     .range([opts.leftPad, opts.width]);
+                    .scale()
+                    .domain([opts.minDate, opts.maxDate])
+                    .range([opts.leftPad, opts.width]);
 
   var labelsScale = d3.scale
                       .ordinal()
@@ -128,41 +126,41 @@ var createChart = function (element, data, opts) {
                      .attr('height', chartHeight);
 
   chartData.append('g')
-       .attr('class', 'xaxis')
-       .attr('transform', 'translate(0,' + (chartHeight + 10) + ')')
-       .call(xAxis);
+           .attr('class', 'xaxis')
+           .attr('transform', 'translate(0,' + (chartHeight + 10) + ')')
+           .call(xAxis);
 
   chartData.append('g')
-       .attr('class', 'yaxis')
-       .attr('transform', 'translate(0, 0)')
-       .call(yAxis);
+           .attr('class', 'yaxis')
+           .attr('transform', 'translate(0, 0)')
+           .call(yAxis);
 
   chartData.selectAll('.yaxis line')
-       .attr('stroke', 'black')
-       .attr('x1', 0)
-       .attr('x2', opts.width)
-       .attr('y1', opts.barHeight / 2)
-       .attr('y2', opts.barHeight / 2);
+           .attr('stroke', 'black')
+           .attr('x1', 0)
+           .attr('x2', opts.width)
+           .attr('y1', opts.barHeight / 2)
+           .attr('y2', opts.barHeight / 2);
 
   chartData.append('g')
-       .selectAll('rect')
-       .data(data)
-       .enter()
-       .append('rect')
-       .attr('class', 'bar')
-       .attr('x', function (d) {
-         return timeScale(new Date(d.startedAt * 1000));
-       })
-       .attr('y', function (d) {
-         return labelsScale(d.label);
-       })
-       .attr('height', opts.barHeight)
-       .attr('width', function (d) {
-         var startedAt = new Date(d.startedAt * 1000);
-         var endedAt = new Date(d.endedAt * 1000);
+           .selectAll('rect')
+           .data(data)
+           .enter()
+           .append('rect')
+           .attr('class', 'bar')
+           .attr('x', function (d) {
+             return timeScale(new Date(d.startedAt * 1000));
+           })
+           .attr('y', function (d) {
+             return labelsScale(d.label);
+           })
+           .attr('height', opts.barHeight)
+           .attr('width', function (d) {
+             var startedAt = new Date(d.startedAt * 1000);
+             var endedAt = new Date(d.endedAt * 1000);
 
-         return timeScale(endedAt) - timeScale(startedAt);
-       });
+             return timeScale(endedAt) - timeScale(startedAt);
+           });
 };
 
 module.exports = createChart;
